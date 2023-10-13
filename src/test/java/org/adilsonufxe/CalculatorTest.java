98fa442d95e3348c7755a0e370518d43c16cb605
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-10-13 11:34:02 +0100

    feat: ensure integer subtraction should result the correct value

--- src/test/java/org/adilsonufxe/CalculatorTest.java
+++ src/test/java/org/adilsonufxe/CalculatorTest.java
@@ -10,4 +10,11 @@ public class CalculatorTest {
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "4 / 2 dit not produce 2");
     }
+
+    @Test
+    void integerSubtraction() {
+        Calculator calculator = new Calculator();
+        int result = calculator.integerSubtraction(5, 2);
+        assertEquals(3, result, "5 - 2 dit not produce 3");
+    }
 }
