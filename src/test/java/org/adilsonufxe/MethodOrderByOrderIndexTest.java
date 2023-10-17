0faf35a001a7dc7719dfbd7637f7964a9d5fa9ed
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-10-17 16:59:14 +0100

    feat: change test lifecycle instance

--- src/test/java/org/adilsonufxe/MethodOrderByOrderIndexTest.java
+++ src/test/java/org/adilsonufxe/MethodOrderByOrderIndexTest.java
@@ -1,23 +1,31 @@
 package org.adilsonufxe;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderByOrderIndexTest {
+
+    StringBuilder completed = new StringBuilder();
+
+    @AfterEach
+    void afterEach() {
+        System.out.println(completed);
+    }
 
     @Order(1)
     @Test
     void testB() {
         System.out.println("Running test B");
+        completed.append("1");
     }
 
     @Order(3)
     @Test
     void testC() {
         System.out.println("Running test C");
+        completed.append("2");
     }
 
 
@@ -25,11 +33,13 @@ public class MethodOrderByOrderIndexTest {
     @Test
     void testD() {
         System.out.println("Running test D");
+        completed.append("2");
     }
 
     @Order(4)
     @Test
     void testA() {
         System.out.println("Running test A");
+        completed.append("2");
     }
 }
