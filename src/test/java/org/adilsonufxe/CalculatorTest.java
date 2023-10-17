40aef97aaa1098e00cf2b9b038ee409a37c6b9e7
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-10-17 11:45:26 +0100

    feat: parameterized test with method source

--- src/test/java/org/adilsonufxe/CalculatorTest.java
+++ src/test/java/org/adilsonufxe/CalculatorTest.java
@@ -3,6 +3,11 @@ package org.adilsonufxe;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @DisplayName("Test Math operations in Calculator class")
 public class CalculatorTest {
@@ -42,17 +47,23 @@ public class CalculatorTest {
         assertEquals("/ by zero", exception.getMessage());
     }
 
-    @Test
-    @DisplayName("Should return Thirty Two When Subtraction ThirtyThree By One  ")
-    void integerSubtraction() {
-        Calculator calculator = new Calculator();
 
-        int minuend = 33;
-        int subtrahend = 1;
-        int expectedResult = 32;
+    @DisplayName("Should return Thirty Two When Subtraction ThirtyThree By One  ")
+    @ParameterizedTest
+    @MethodSource()
+    void integerSubtraction(int minuend, int subtrahend, int expectedResult) {
+        Calculator calculator = new Calculator();
 
         int result = calculator.integerSubtraction(minuend, subtrahend);
 
         assertEquals(expectedResult, result, () -> minuend + " - " + subtrahend + " did not produce " + expectedResult);
+    }
+
+    private static Stream<Arguments> integerSubtraction() {
+        return Stream.of(
+                Arguments.of(33, 1, 32),
+                Arguments.of(20, 21, -1),
+                Arguments.of(10, 10, 0)
+        );
     }
 }
