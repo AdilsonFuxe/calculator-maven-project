7c77780db3042b93f5ca560ef79849615ebf4ac4
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-10-17 15:55:31 +0100

    feat: run test order by method order

--- src/test/java/org/adilsonufxe/MethodOrderByOrderIndexTest.java
+++ src/test/java/org/adilsonufxe/MethodOrderByOrderIndexTest.java
@@ -1,28 +1,33 @@
 package org.adilsonufxe;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
-public class MethodOrderByNameTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderByOrderIndexTest {
 
+    @Order(1)
     @Test
     void testB() {
         System.out.println("Running test B");
     }
 
+    @Order(3)
     @Test
     void testC() {
         System.out.println("Running test C");
     }
 
 
+    @Order(2)
     @Test
     void testD() {
         System.out.println("Running test D");
     }
 
+    @Order(4)
     @Test
     void testA() {
         System.out.println("Running test A");
